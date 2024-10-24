be92d8417f3ba79db8b1e85bd1e8132fb33dce19
MAC\da <daniilantonenko@ya.ru>
2024-10-24 21:55:31 +0300

    Clear code

--- parse.py
+++ parse.py
@@ -1,24 +1,5 @@
 import re
-
-def parse_price(price_str):
-    match = re.search(r'(\d[\d\s]*)', price_str)
-    if match:
-        price = match.group(1)
-        price = price.replace(' ', '')
-        return price
-    else:
-        return None
     
-def parse_article(article_str):
-    match = re.search(r'(\d[\d\w]*)', article_str)
-    if match:
-        article = match.group(1)
-        article = article.replace(' ', '')
-        return article
-    else:
-        return None
-    
-
 def parse(string: str, template:str) -> str:
     """
     :param string: string contains data
