9da4d2da496d933ce426c5e32e9c2ec6f132aff2
MAC\da <daniilantonenko@ya.ru>
2024-10-29 10:11:44 +0300

    Get data from json

--- main.py
+++ main.py
@@ -1,7 +1,42 @@
 from models import *
-   
+import json
+import copy
+
+def load_json(filename):
+    with open(filename) as f:
+        d = json.load(f)
+        return d
+
+def parse_to_models(data):
+    for key, value in data.items():
+        if key == 'Organization':
+            for item in value:
+
+                item_clear = copy.deepcopy(item)
+                key_to_remove = 'Proffile'
+                if key_to_remove in item_clear:
+                    item_clear.pop(key_to_remove)
+                
+                key_to_remove2 = 'Urls'
+                if key_to_remove2 in item_clear:
+                    item_clear.pop(key_to_remove2)
+
+                org_id = Organization.create_or_update(**item_clear)
+                #print("organization:", org_id)
+
+                for k, v in item.items():
+                    if k == 'Proffile':
+                        for p in v:
+                            proffile_id = Proffile.create_or_update(organization=org_id,**p)
+                            #print("proffile:", proffile_id)
+                    elif k == 'Urls':
+                        for u in v:
+                            page_id = Page.create_or_update(organization=org_id, url=u)
+                            #print("page:", page_id)
+
 def scan(urls, organization):
     for url in urls:
+        #print(organization, url)
         page = Page.create_or_update(organization=organization, url=url)
         if page is not None and page > 0:
             p = Page.get(id=page)
@@ -17,78 +52,16 @@ def scan(urls, organization):
             print(f"Page creation failed for {url}")
             return
 
-org = Organization.create_or_update(
-     name='STAN', 
-     domain='https://www.stan.su')
+def multi_scan():
+    orgs = Organization.select()
+    for org in orgs:
+        urls = org.pages
+        urls_list = [url.url for url in urls]
+        scan(urls_list, org)
 
-prof_price = Proffile.create_or_update(
-    organization=org,
-    name='price',
-    tag='meta',
-    attribute='itemprop',
-    value='price',
-    template=r'(\d[\d\s]*)',
-    value_attribute='content'
-)
+j = load_json('data.json')
+parse_to_models(j)
 
-proffile_article = Proffile.create_or_update(
-    organization=org,
-    name='article',
-    tag='span',
-    attribute='class',
-    value='article_element',
-    template=r'(\d[\d\w]*)'
-)
-
-proffile_name = Proffile.create_or_update(
-    organization=org,
-    name='name',
-    tag='h1',
-    attribute='id',
-    value='pagetitle',
-    template=r"^(.*?)\sSTAN\s(.*?)(?=,)"
-)
-     
-proffile_image = Proffile.create_or_update(
-    organization=org,
-    name='image',
-    tag='img',
-    attribute='class',
-    value='product-detail-gallery__picture',
-    value_attribute='data-src'
-)  
-
-proffile_charecteristics_table = Proffile.create_or_update(
-    organization=org,
-    name='characteristics_table',
-    tag='table',
-    attribute='class',
-    value='props_list'
-)
-
-proffile_charecteristics_name = Proffile.create_or_update(
-    organization=org,
-    name='characteristics_name',
-    tag='span',
-    attribute='itemprop',
-    value='value',
-    disable=["Торговая марка","Цвет основ."]
-)
-
-proffile_charecteristics_value = Proffile.create_or_update(
-    organization=org,
-    name='characteristics_value',
-    tag='span',
-    attribute='itemprop',
-    value='value'
-)
-
-test_urls = ['https://www.stan.su/catalog/odezhda_1/bombery_i_olimpiyki/bomber_70/898642/',
-            'https://www.stan.su/catalog/odezhda_1/bombery_i_olimpiyki/bomber_71/898754/',
-            'https://www.stan.su/catalog/odezhda_1/bombery_i_olimpiyki/tolstovka_95/803848/',
-            'https://www.stan.su/catalog/odezhda_1/bombery_i_olimpiyki/tolstovka_65n/799055/',
-            'https://www.stan.su/catalog/odezhda_1/bombery_i_olimpiyki/tolstovka_65/202007/']
-
-scan(test_urls, org)
+multi_scan()
 
 # TODO: catalog scanner
