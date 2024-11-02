398f2b919e4026683b341c218cd125049e87bc5b
MAC\da <daniilantonenko@ya.ru>
2024-11-02 16:02:54 +0300

    Add print status

--- main.py
+++ main.py
@@ -5,9 +5,12 @@ from parser import *
 async def main():
     # Load new data to database
     j = load_json('data.json')
+
+    print('Saving data to database')
     await save_to_database(j)
 
     # Scan all Organizations
+    print('Scanning all Organizations')
     await scan_all()
 
 asyncio.run(main())
